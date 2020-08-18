d56906b7cda4b075dc30ab36db8391886fe06107
Luciano Liboni <luciano_liboni@hotmail.com>
2020-08-18 12:25:46 -0300

    fix: por connection

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -4,7 +4,7 @@ const db = knex({
   client: 'pg',
   connection: {
     host: process.env.DB_HOST,
-    port: 5434,
+    port: 5432,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
