56eb7a9b949bf500587a14fbe67e3cd495341032
Luciano Liboni <luciano_liboni@hotmail.com>
2020-08-18 12:05:36 -0300

    connection

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,23 +1,15 @@
 import knex from 'knex';
 
-let db = {};
-
-(async () => {
-  try {
-    db = knex({
-      client: 'pg',
-      connection: {
-        host: process.env.DATABASE_URL,
-        // port: 5434,
-        // user: process.env.DB_USER,
-        // password: process.env.DB_PASSWORD,
-        // database: process.env.DB_DATABASE
-      },
-      useNullAsDefault: true,
-    });
-  } catch(err){
-    console.log('Erro ao realizar conexão ao BD: ', err);
-  }
-})();
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: process.env.DATABASE_URL,
+    // port: 5434,
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_DATABASE
+  },
+  useNullAsDefault: true,
+})
 
 export default db;
